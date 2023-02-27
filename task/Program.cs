71964954a69b7f2651282b5aa651b2677c4f63d7
Aleksey <lexandreeev@gmail.com>
2023-02-27 15:06:33 +0300

    Написали код для данной программы

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayOne = new string[5] {"123", "=)", "hello", "world", "str"};
+string[] arrayTwo = new string[arrayOne.Length];
+void SecondArrayWithIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIf(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
